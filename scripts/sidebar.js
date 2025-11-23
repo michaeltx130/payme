a60bd4e278e867f1f666a0dceb526bd377ade3f5
Michelle <114263903+michaeltx130@users.noreply.github.com>
2025-11-22 20:09:27 -0700

    Cambio en la interfaz y mejoras

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,51 +1,49 @@
 //Mostrar menú en todas las pantallas
 function loadSidebar() {
-    fetch('sidebar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al cargar la barra lateral');
-            }
-            return response.text();
-        })
-        .then(html => {
-            document.querySelector('.sidebar-container').innerHTML = html;
+  fetch("sidebar.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al cargar la barra lateral");
+      }
+      return response.text();
+    })
+    .then((html) => {
+      document.querySelector(".sidebar-container").innerHTML = html;
 
-            initializeSidebarNavigation();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+      initializeSidebarNavigation();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
-
 function initializeSidebarNavigation() {
-    const currentPath = window.location.pathname.split('/').pop();
-    const buttons = document.querySelectorAll('.btns-menu button');
+  const currentPath = window.location.pathname.split("/").pop();
+  const buttons = document.querySelectorAll(".btns-menu button");
 
-    buttons.forEach(button => {
-        const link = button.getAttribute('data-link');
+  buttons.forEach((button) => {
+    const link = button.getAttribute("data-link");
 
-        if (link === currentPath) {
-            button.classList.add('active');
-        }
-
-        button.addEventListener('click', () => {
-            navigateTo(link);
-        });
-    });
-
-
-    const logo = document.querySelector('.sidebar h1');
-    if (logo) {
-        const dashboardLink = logo.getAttribute('data-link');
-        logo.addEventListener('click', () => {
-            navigateTo(dashboardLink);
-        });
+    if (link === currentPath) {
+      button.classList.add("active");
     }
+
+    button.addEventListener("click", () => {
+      navigateTo(link);
+    });
+  });
+
+  const logo = document.querySelector(".sidebar-logo");
+  if (logo) {
+    const dashboardLink = logo.getAttribute("data-link");
+    logo.addEventListener("click", () => {
+      navigateTo(dashboardLink);
+    });
+  }
 }
 
 function navigateTo(url) {
-    window.location.href = url;
+  window.location.href = url;
 }
 
-document.addEventListener('DOMContentLoaded', loadSidebar);
+document.addEventListener("DOMContentLoaded", loadSidebar);
--- scripts/window_floating.js
+++ scripts/window_floating.js
@@ -1,10 +1,18 @@
 const showWindowBtn = document.getElementById('showWindowBtn');
+const showCardWindowBtn = document.getElementById('showCardWindowBtn');
 const floatingWindow = document.getElementById('floatingWindow');
+const floatingCardWindow = document.getElementById('floatingCardWindow');
 const closeWindowBtn = document.getElementById('closeWindowBtn');
+const closeCardWindowBtn = document.getElementById('closeCardWindowBtn');
 const overlay = document.getElementById('overlay');
 
 showWindowBtn.addEventListener('click', () => {
     floatingWindow.style.display = 'block';
+    overlay.style.display = 'block';
+});
+
+showCardWindowBtn.addEventListener('click', () => {
+    floatingCardWindow.style.display = 'block';
     overlay.style.display = 'block';
 });
 
@@ -13,7 +21,13 @@ closeWindowBtn.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
+closeCardWindowBtn.addEventListener('click', () => {
+    floatingCardWindow.style.display = 'none';
+    overlay.style.display = 'none';
+});
+
 overlay.addEventListener('click', () => {
     floatingWindow.style.display = 'none';
+    floatingCardWindow.style.display = 'none';
     overlay.style.display = 'none';
 });
